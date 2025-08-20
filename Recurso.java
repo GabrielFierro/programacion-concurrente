68072938234499ea12370e0dda25edec88d74111
GabrielFierro <fierrogabriel13@gmail.com>
2025-08-20 19:02:57 -0300

    add a comment

--- Recurso.java
+++ Recurso.java
@@ -1,5 +1,6 @@
 public class Recurso {
     static void uso() {
+        // Crea las instancias de tipo Thread
         Thread t = Thread.currentThread();
         System.out.println("en Recurso: Soy " + t.getName());
     }
