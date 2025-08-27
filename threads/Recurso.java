c74e51c4f89385251618cd5049136eaa0ac78b2d
GabrielFierro <fierrogabriel13@gmail.com>
2025-08-27 13:44:38 -0300

    make public uso method

--- threads/Recurso.java
+++ threads/Recurso.java
@@ -1,7 +1,7 @@
 package threads;
 
 public class Recurso {
-    static void uso() {
+    public static void uso() {
         // Crea las instancias de tipo Thread
         Thread t = Thread.currentThread();
         System.out.println("en Recurso: Soy " + t.getName());
